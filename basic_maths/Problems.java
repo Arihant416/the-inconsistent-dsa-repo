abd519726046ae172f28cb5abd6b1c593fb57cb8
Arihant416 <arihantjain416@gmail.com>
2024-12-12 17:54:12 +0530

    Count of odd digits in number

--- basic_maths/Problems.java
+++ basic_maths/Problems.java
@@ -1,12 +1,16 @@
+
 import java.util.Scanner;
 
 public class Problems {
 
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-          int number = sc.nextInt();
-          // Problem 1: Count Digits in a number
-          System.out.println("Digits in number -> " + number + " is " + countDigits(number));
+            int number = sc.nextInt();
+            // Problem 1: Count Digits in a number
+            System.out.println("Digits in number -> " + number + " is " + countDigits(number));
+
+            // Problem 2: Count Number of odd digits in a number
+            System.out.println("Odd Digits in number -> " + number + " is " + countOddDigits(number));
         }
     }
 
@@ -14,6 +18,18 @@ public class Problems {
         int count = 0;
         while (num > 0) {
             count += 1;
+            num /= 10;
+        }
+        return count;
+    }
+
+    public static int countOddDigits(int num) {
+        int count = 0;
+        while (num > 0) {
+            int rem = num % 10;
+            if (rem % 2 != 0) {
+                count++;
+            }
             num /= 10;
         }
         return count;
