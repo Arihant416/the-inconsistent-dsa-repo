30ca437c256f3dcc11b8cb15953919a703678fb4
Arihant416 <arihantjain416@gmail.com>
2024-12-13 10:04:32 +0530

    Highest Occurring Element Done

--- Hashing/BH1.java
+++ Hashing/BH1.java
@@ -1,4 +1,5 @@
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 class BH1 {
@@ -12,6 +13,7 @@ class BH1 {
             nums[i] = sc.nextInt();
         }
         System.out.println("Largest element in the array is " + findLargestElement(nums));
+        System.out.println("Highest Occurring element in array is " + findHighestOccurringElement(nums));
     }
 
     private static int findLargestElement(int[] nums) {
@@ -22,5 +24,22 @@ class BH1 {
             }
         }
         return maxNum;
+    }
+
+    private static Integer findHighestOccurringElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int el : nums) {
+            int count = map.getOrDefault(el, 0);
+            map.put(el, count + 1);
+        }
+        int maxCount = 0, element = 0;
+        for (Integer key : map.keySet()) {
+            int val = map.get(key);
+            if (val > maxCount) {
+                maxCount = val;
+                element = key;
+            }
+        }
+        return element;
     }
 }
