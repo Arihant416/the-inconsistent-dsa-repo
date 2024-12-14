cbb7c0fc19662830d74cb9ddd63350e43893a252
Arihant416 <arihantjain416@gmail.com>
2024-12-14 15:18:38 +0530

    Print 1 to N, N to 1

--- BasicRecursion/BasicProblems.java
+++ BasicRecursion/BasicProblems.java
@@ -15,6 +15,8 @@ class BasicProblems {
         headRecursion(n);
         duration = System.currentTimeMillis() - stHR;
         System.out.println("Head Recursion took " + duration);
+        print1ToN(n);
+        printNTo1(n);
     }
 
     // This is a case for tail recursion
@@ -36,5 +38,19 @@ class BasicProblems {
         for (int i = 1; i < 1000; i++) {
         }
         // System.out.println("Head Recursion " + n);
+    }
+
+    private static void print1ToN(int n) {
+        if (n == 0) {
+            return;
+        }
+        print1ToN(n - 1);
+        System.out.println(n);
+    }
+
+    private static void printNTo1(int n) {
+        if(n == 0)return;
+        System.out.println(n);
+        printNTo1(n - 1);
     }
 }
