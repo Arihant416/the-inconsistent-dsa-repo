be7f380277f420601607e875eb6245d6e116639b
Arihant416 <arihantjain416@gmail.com>
2024-12-14 21:39:27 +0530

    Selection Sort Done

---
+++ SortingAlgorithms/SelectionSort.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SelectionSort {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int size = sc.nextInt();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++) {
+      array[i] = sc.nextInt();
+    }
+    selectionSort(array, size);
+    for (int element : array) {
+      System.out.print(element + " ");
+    }
+  }
+
+  private static void selectionSort(int[] nums, int size) {
+    for (int i = 0; i < size; i++) {
+      int currentMinIndex = i;
+      int j = i + 1;
+      while (j < size) {
+        if (nums[j] < nums[currentMinIndex]) {
+          currentMinIndex = j;
+        }
+        j++;
+      }
+      int temp = nums[i];
+      nums[i] = nums[currentMinIndex];
+      nums[currentMinIndex] = temp;
+    }
+  }
+}
