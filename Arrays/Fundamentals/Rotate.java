56162db30e2bb396c2ab5b16e180bccc33804cdb
Arihant416 <arihantjain416@gmail.com>
2024-12-15 15:15:41 +0530

    Rotation By K also done for left

--- Arrays/Fundamentals/Rotate.java
+++ Arrays/Fundamentals/Rotate.java
@@ -10,7 +10,10 @@ class Rotate {
     for (int i = 0; i < N; i++) {
       nums[i] = sc.nextInt();
     }
+    int K = sc.nextInt();
     leftRotateByOne(nums, N);
+    rightRotateByOne(nums, N);
+    leftRotateByK(nums, N, K);
   }
 
   private static void reverse(int[] nums, int startIndex, int endIndex) {
@@ -26,23 +29,39 @@ class Rotate {
   private static void print(int[] nums) {
     for (int el : nums)
       System.out.print(el + " ");
+    System.out.println();
   }
 
   private static void leftRotateByOne(int[] nums, int N) {
     int[] nums1 = nums.clone();
-    System.out.println("Before Reverse ");
+    System.out.println("Left Rotation By One\nBefore Rotate ");
     print(nums1);
     reverse(nums1, 0, N - 1);
     reverse(nums1, 0, N - 2);
-    System.out.println("\nAfter Reverse ");
+    System.out.println("After Rotate ");
     print(nums1);
   }
 
-  private static void leftRotateByK(int[] nums, int N) {
+  private static void leftRotateByK(int[] nums, int N, int K) {
+    int[] nums1 = nums.clone();
+    K = K % N;
+    System.out.println("Left Rotation By K\nBefore Rotate");
+    print(nums1);
+    reverse(nums1, 0, K - 1);
+    reverse(nums1, K, N - 1);
+    reverse(nums1, 0, N - 1);
+    System.out.println("After Rotate ");
+    print(nums1);
   }
 
   private static void rightRotateByOne(int[] nums, int N) {
-
+    int[] nums1 = nums.clone();
+    System.out.println("Right Rotation By One\nBefore Rotate ");
+    print(nums1);
+    reverse(nums1, 0, N - 1);
+    reverse(nums1, 1, N - 1);
+    System.out.println("After Rotate ");
+    print(nums1);
   }
 
   private static void rightRotateByK(int[] nums, int N) {
