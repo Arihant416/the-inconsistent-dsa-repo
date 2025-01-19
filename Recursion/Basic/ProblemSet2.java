d883c6cd674e1f575866ca8feae2cf8473175aa5
Arihant416 <arihantjain416@gmail.com>
2025-01-19 13:09:41 +0530

    Print All Subsequence

--- Recursion/Basic/ProblemSet2.java
+++ Recursion/Basic/ProblemSet2.java
@@ -1,5 +1,28 @@
-class ProblemSet2{
+import java.util.ArrayList;
+import java.util.List;
+
+class ProblemSet2 {
   public static void main(String[] args) {
-    
+    int[] A = new int[] { 3, 1, 2 };
+    printAllSubsequences(A);
+  }
+
+  static void printAllSubsequencesHelper(int[] A, List<Integer> curList, int index) {
+    if (index >= A.length) {
+      for (Integer i : curList) {
+        System.out.print(i + " ");
+      }
+      System.out.println();
+      return;
+    }
+    curList.add(A[index]);
+    printAllSubsequencesHelper(A, curList, index + 1);
+    curList.removeLast();
+    printAllSubsequencesHelper(A, curList, index + 1);
+  }
+
+  static void printAllSubsequences(int[] A) {
+    List<Integer> curList = new ArrayList<>();
+    printAllSubsequencesHelper(A, curList, 0);
   }
 }
