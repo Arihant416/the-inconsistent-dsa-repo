5af82bebdfeb38c6d51dca89deb212cff0cca52c
Arihant416 <arihantjain416@gmail.com>
2025-01-19 18:00:31 +0530

    Subsequence with Sum k any one

--- Recursion/Basic/ProblemSet2.java
+++ Recursion/Basic/ProblemSet2.java
@@ -2,16 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 class ProblemSet2 {
+
   public static void main(String[] args) {
     int[] A = new int[] { 1, 2, 3, 4 };
     int sum = 4;
     printAllSubsequences(A);
     printSubsequencesWhoseSumIsK(A, sum);
+    printAnyOneSubsequenceWithSumK(A, sum);
   }
 
   static void printAllSubsequencesHelper(int[] A, List<Integer> curList, int index) {
     if (index >= A.length) {
-      if (curList.size() == 0) {
+      if (curList.isEmpty()) {
         System.out.println("{}");
       } else {
         for (Integer i : curList) {
@@ -55,5 +57,27 @@ class ProblemSet2 {
     List<Integer> curList = new ArrayList<>();
     System.out.println("Subsequences with sum K : ");
     subsequencesWithSumK(A, sum, curList, 0, 0);
+  }
+
+  static boolean anyOneSubsequenceWithSumK(int[] A, int sum, List<Integer> curList, int index) {
+    if (index == A.length) {
+      return sum == 0;
+    }
+    sum -= A[index];
+    curList.add(A[index]);
+    if (anyOneSubsequenceWithSumK(A, sum, curList, index + 1)) {
+      return true;
+    }
+    curList.remove(curList.size() - 1);
+    sum += A[index];
+    return (anyOneSubsequenceWithSumK(A, sum, curList, index + 1));
+  }
+
+  static void printAnyOneSubsequenceWithSumK(int[] A, int sum) {
+    List<Integer> curList = new ArrayList<>();
+    if (anyOneSubsequenceWithSumK(A, sum, curList, 0))
+      System.out.println("Found one subsequence " + curList.toString());
+    else
+      System.out.println("Not found any one subsequence with sum k");
   }
 }
